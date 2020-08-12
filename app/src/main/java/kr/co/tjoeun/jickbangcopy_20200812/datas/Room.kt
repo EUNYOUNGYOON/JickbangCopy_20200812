565dcacd3df5d105ca76121b58229c0126e9d5c0
EUNYOUNGYOON <milkoon1@naver.com>
2020-08-12 20:08:03 +0900

    Room 데이터를 표현 클래스 (멤버변수 4개와 함께) 생성

---
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/datas/Room.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.jickbangcopy_20200812.datas
+
+import java.net.Inet4Address
+
+//2. ListView가 뿌려줄 데이터들을 묶어서 표현하는 데이터 클래스 작업
+class Room (
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val description: String
+){
+}
