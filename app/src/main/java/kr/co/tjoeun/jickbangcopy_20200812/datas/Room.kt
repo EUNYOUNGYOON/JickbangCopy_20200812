6361c354cc89614dd31b88f69347776f89eb206d
EUNYOUNGYOON <milkoon1@naver.com>
2020-08-12 20:35:37 +0900

    room 목록 표현 xml 추가

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/datas/Room.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/datas/Room.kt
@@ -4,6 +4,7 @@ import java.net.Inet4Address
 
 //2. ListView가 뿌려줄 데이터들을 묶어서 표현하는 데이터 클래스 작업
 class Room (
+    //클래스의 생성자에서 val 변수이름: 자료형 나열해서 클래스가 가져야하는 정보 항목들로 설정
     val price: Int,
     val address: String,
     val floor: Int,
