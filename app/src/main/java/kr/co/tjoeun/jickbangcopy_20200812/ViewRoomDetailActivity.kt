c361e476eb5e09f2f957ac07ee24fe2c44bb4c90
EUNYOUNGYOON <milkoon1@naver.com>
2020-08-13 21:51:44 +0900

    View상세보기 주석 완료

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/ViewRoomDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200812/ViewRoomDetailActivity.kt
@@ -21,10 +21,14 @@ class ViewRoomDetailActivity : BaseActivity() {
     override fun setValues() {
 
         //8-4) 데이터 담기
+        // 메인화면에서 넘겨준 Room 가져오기
+        // 넘겨줄 때 Serializable 형태로 넘겨줌 -> Room으로 변환해서 지정해야 사용 가능.
+        // as Room 추가
         val room = intent.getSerializableExtra("roomInfo") as Room
 
         // 8-6) as Room 캐스팅이 안하면 에러가 난다.
         // 9-2) getFormattedPrice 적용
+        // 실제 데이터 반영
         priceTxt.text = room.getFormattedPrice()
         addressTxt.text = room.address
         descTxt.text = room.description
